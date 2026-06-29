7f776fc09fa16961e3d201410084bb2017d5bf66
Heorhii Virchenko <heorhiivirchenko@MacBook-Air-von-Heorhii.local>
2026-06-30 00:20:43 +0200

    your message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useRef, useEffect, useCallback } from "react";
+import React, { useState, useMemo, useRef, useEffect } from "react";
 import { supabase } from "./lib/supabase";
 import {
   loadUserData, saveUserData,
